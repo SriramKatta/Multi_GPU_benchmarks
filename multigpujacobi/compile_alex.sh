b5b73952607365e15b75afa7bcabfd3f631079f9
Sriram <87057612+SriramKatta@users.noreply.github.com>
2026-01-19 08:50:22 +0100

    Swap comments for NCCL and NVSHMEM libraries

--- multigpujacobi/compile_alex.sh
+++ multigpujacobi/compile_alex.sh
@@ -6,10 +6,10 @@ module load nvhpc
 
 
 export NV_COMM_LIBS=$NVHPC_ROOT/Linux_x86_64/25.5/comm_libs
-#load nvshmem library 
+#load nccl library  
 export NCCL_HOME=$NV_COMM_LIBS/nccl
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$NCCL_HOME/lib
-#load nccl library 
+#load nvshmem library
 export NVSHMEM_HOME=$NV_COMM_LIBS/nvshmem
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$NVSHMEM_HOME/lib
 
